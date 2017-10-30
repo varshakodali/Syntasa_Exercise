9cc9897b69a613211ef08fe72ea37f19b5937182
varshakodali <varshak180@gmail.com>
2017-10-29 19:50:32 -0700

    Final solution

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,38 +2,48 @@ var fs = require('fs');
 var fileNames = ['file1.txt','file2.txt'];
 var promises = [];
 
-for (var i = 0; i < fileNames.length; i++) {
-    promises.push( 
-    	new Promise((resolve, reject) => {
-    		fs.readFile(fileNames[i], 'utf-8', function(error, data){
-            	
-            	let promiseResult = {};
-	            if(error) {
-	            	promiseResult.isRejected = true;
-	            	promiseResult.isResolved = false;
-	            	promiseResult.reason = error.message;
-	            	reject(promiseResult);
-	            }
-	            else{
-	            	promiseResult.isRejected = false;
-	            	promiseResult.isResolved = true;
-	            	promiseResult.value = data;
-	            	resolve(promiseResult);
-	            }         
-        	});
-    	}).catch(function(error){
-    		return error;
-    	})
-    );
+function promiseResolver() {
+
+  return new Promise(function(resolve, reject) {
+
+    for (var i = 0; i < fileNames.length; i++) {
+        promises.push( 
+          new Promise((resolve, reject) => {
+            fs.readFile(fileNames[i], 'utf-8', function(error, data){
+                  
+                  let promiseResult = {};
+                  if(error) {
+                    promiseResult.isRejected = true;
+                    promiseResult.isResolved = false;
+                    promiseResult.reason = error.message;
+                    reject(promiseResult);
+                  }
+                  else{
+                    promiseResult.isRejected = false;
+                    promiseResult.isResolved = true;
+                    promiseResult.value = data;
+                    resolve(promiseResult);
+                  }         
+              });
+          }).catch(function(error){
+            return error;
+          })
+        );
+    }
+
+    Promise.all(promises)
+      .then(result => {
+        return new Promise((resolve, reject) => {
+          resolve(result);
+        });
+      })
+      .then(function(arr){
+        resolve(arr);
+      }) 
+      .catch(err => console.log('Catch', err));
+  });
 }
 
-Promise.all(promises)
-  .then(result => {
-  	return new Promise((resolve, reject) => {
-  		resolve(result);
-  	});
-  })
-  .then(function(arr){
-  	console.log("Promise<Array<promiseResult>>:\n", arr);
-  }) 
-  .catch(err => console.log('Catch', err));
+promiseResolver().then(function(r){
+  console.log(r);
+})
